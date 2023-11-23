027437b554850676aa99a70abdac2134a72d3d5e
davem1988 <davidgaudreau1988@gmail.com>
2023-11-23 10:09:50 -0500

    update status command

--- commands/fivem/status.js
+++ commands/fivem/status.js
@@ -1,4 +1,8 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+require('dotenv').config();
+const FiveM = require("fivem")
+const IP = process.env.SERVER_IP
+const PORT = process.env.SERVER_PORT
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,12 +10,20 @@ module.exports = {
         .setDescription('Voir le status du serveur'),
     usage: '/ip',
     async execute(interaction) {
+        const server = new FiveM.Server(`${IP}:${PORT}`)
+
         const StatusEmbed = new EmbedBuilder()
-        .setTitle('Commande: /status')
-        .setColor('Red')
-        .setDescription('Le serveur est MORT à cause de Rene!!!')
+        .setTitle('Status du Serveur')
+        .setColor('Blue')
+        .setDescription('Status actuel du serveur')
         .setTimestamp()
         .setAuthor({ name: `${interaction.client.user.username}`})
+
+        server.getServerStatus().then(status => {
+            console.log(status)
+        })
+
+
 
         
         return await interaction.reply({embeds: [StatusEmbed]});
