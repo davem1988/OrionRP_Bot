dca2f7c588447d9d2ba91504c3933af0beb76338
davem1988 <davidgaudreau1988@gmail.com>
2023-11-29 09:50:24 -0500

    add event guildMemberRemove

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,24 @@
+const { Events, EmbedBuilder } = require('discord.js');
+const channelId = '1165065277155848284'
+module.exports = {
+    name: Events.GuildMemberAdd,
+    once: false,
+    execute(client, member) {
+        const channel = client.channels.cache.get(channelId);
+        const now = new Date()
+
+        const newMemberEmbed =  new EmbedBuilder()
+        .setTitle('Un membre vient de quitter')
+        .setDescription(
+            `**Username**: ${member.displayName}\n
+             **Membre depuis**: <t:${parseInt(member.user.joinedTimestamp / 1000)}:R>\n
+             **Discord ID**:  ${member.user.id}\n
+             `
+        )
+        .setColor('Red')
+        .setThumbnail(member.user.displayAvatarURL())
+        .setTimestamp()
+
+        channel.send({embeds: [newMemberEmbed]});
+    },
+};
