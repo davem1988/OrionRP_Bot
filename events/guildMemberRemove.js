4c99a15bc0a41bd97404e42eb9b35a88e2fd692b
davem1988 <davidgaudreau1988@gmail.com>
2023-11-29 10:10:41 -0500

    fix member since in guildMemberRemove 2

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -6,7 +6,7 @@ module.exports = {
     execute(client, member) {
         const channel = client.channels.cache.get(channelId);
         const now = new Date()
-        const timeDifference = now - member.user.joinedAt;
+        const timeDifference = now - member.joinedAt;
 
         const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
         const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
